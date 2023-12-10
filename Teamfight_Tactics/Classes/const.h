f708e802eb38dfb59f384f9ce8cc0e208dda5ad2
LAPTOP-7L7D8RU5\A001 <2253215@tongji.edu.cn>
2023-12-10 23:23:57 +0800

    hero基类，常量头文件const.h的第一次更新

---
+++ Teamfight_Tactics/Classes/const.h
@@ -0,0 +1,22 @@
+//本头文件存储的为所有人物参数
+#pragma once
+#include <cocos2d.h>
+using namespace cocos2d;
+
+
+namespace heroConsts {
+
+enum heroType
+{
+	at_null=1, at_player, at_ai
+};//英雄类型分为玩家与电脑玩家
+
+const int max_health_point = 100;//设置英雄血量为100
+const int max_blue_point = 100;//设置英雄蓝条为100
+const int max_defence_point = 100;//设置英雄护盾值为100
+
+
+
+
+
+}
